d3216bc2b6b27f1a31c61c41f6e2b90c802ae0e5
laraturrion <laturrions@gmail.com>
2024-01-13 13:51:39 +0100

    importacion ambigua

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 
+import './App.css'
+import WeatherApp from './components/WeatherApp';
 
-export const App = () => {
-  return (
-    <></>
-    
-  )
+function App () {
+return (
+  <div className="App">
+  <WeatherApp/>
+  </div>
+)
 }
 
 export default App;
-
--- src/components/WeatherApp.jsx
+++ src/components/WeatherApp.jsx
@@ -1,1 +1,15 @@
-import "/WeatherApp.css"
+import "./weatherApp.css";
+
+
+const WeatherApp = () => {
+  return (
+    <div className="container">
+      <nav className="nav">
+        <input type="text" className="cityInput" placeholder="Search" />
+        <div className="searchIcon"></div>
+      </nav>
+    </div>
+  );
+};
+
+export default WeatherApp;
